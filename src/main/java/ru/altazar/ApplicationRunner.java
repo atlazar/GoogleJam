8a4e578c117de9e7768541951dfcd9d9a7ddcedc
Andrey Azarov <azarov.andrey@gmail.com>
2018-04-04 23:17:18 +0300

    Switch to Gradle. Remove Guava

--- src/main/java/ru/altazar/ApplicationRunner.java
+++ src/main/java/ru/altazar/ApplicationRunner.java
@@ -1,41 +1,33 @@
 package ru.altazar;
 
-import com.google.common.reflect.ClassPath;
-import com.google.common.reflect.ClassPath.ClassInfo;
+import ru.altazar.contest2016.round1a.BestFriendForever;
 
 import java.io.*;
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Modifier;
+import java.util.HashMap;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 public class ApplicationRunner {
 
+    private static final Map<String, Problem> PROBLEMS = new HashMap<>();
 
-    public static void main(String[] args) throws Exception {
+    static {
+        register(new BestFriendForever());
+    }
 
-        ClassPath classPath = ClassPath.from(ApplicationRunner.class.getClassLoader());
+    private static void register(Problem problem) {
+        PROBLEMS.put(problem.getClass().getSimpleName().toLowerCase(), problem);
+    }
 
-        Map<String, ? extends Problem> problems = classPath
-                .getTopLevelClassesRecursive("ru.altazar")
-                .stream()
-                .map(ClassInfo::load)
-                .filter(ApplicationRunner::isConcreteProblem)
-                .filter(ApplicationRunner::hasDefaultConstructor)
-                .collect(Collectors.toMap(
-                        aClass -> aClass.getSimpleName().toLowerCase(),
-                        ApplicationRunner::instantiate
-                ));
+    public static void main(String[] args) {
 
         if (args.length < 1) {
-            System.err.println("Please specify problem to solve as first argument. Known problems are: " + problems.keySet());
+            System.err.println("Please specify problem to solve as first argument. Known problems are: " + PROBLEMS.keySet());
             return;
         }
 
-        Problem problem = problems.get(args[0]);
+        Problem problem = PROBLEMS.get(args[0]);
         if (problem == null) {
-            System.err.println("Unknown problem " + args[0] + ". Known problems are: " + problems.keySet());
+            System.err.println("Unknown problem " + args[0] + ". Known problems are: " + PROBLEMS.keySet());
             return;
         }
 
@@ -46,26 +38,4 @@ public class ApplicationRunner {
             e.printStackTrace();
         }
     }
-
-    private static Problem instantiate(Class<?> aClass) {
-        try {
-            return (Problem) aClass.getConstructor().newInstance();
-        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private static boolean hasDefaultConstructor(Class<?> aClass) {
-        for (Constructor<?> constructor : aClass.getConstructors()) {
-            if (constructor.getParameterCount() == 0 && Modifier.isPublic(constructor.getModifiers())) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private static boolean isConcreteProblem(Class<?> aClass) {
-        return aClass != Problem.class && Problem.class.isAssignableFrom(aClass);
-    }
-
 }
